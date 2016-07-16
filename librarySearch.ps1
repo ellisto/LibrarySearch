46185f6219d0689af226774bb6e5ca8144584f4b
TJ Ellis <TJ Ellis>
2016-07-16 12:16:30 -0400

    cleaned up format

--- librarySearch.ps1
+++ librarySearch.ps1
@@ -1,63 +1,68 @@
 ﻿param(
-       [string]$isbn="9780143038276",
-       [string]$library="Silver Spring")
+        [string]$isbn="9780143038276",
+        [string]$library="Silver Spring")
 function CheckLibrary
-{
-    
+{    
     param(
-       [string]$isbn="9780062351425",
-       [string]$library="Silver Spring")
+            [string]$isbn="9780062351425",
+            [string]$library="Silver Spring")
 
-     $LibraryIds = @{}
+    $LibraryIds = @{}
 
-     $url = "http://mdpl.ent.sirsi.net/client/catalog/search/results?qu=$isbn"
- 
-     $results = Invoke-WebRequest $url
-     if($results -match "This search returned no results."){return $false}
+    $url = "http://mdpl.ent.sirsi.net/client/catalog/search/results?qu=$isbn"
 
-     #get availabilityDiv ids for all libraries that have a copy
-     $trs = $results.ParsedHtml.getElementsByTagName("tr")
-     if($trs[0].innerHTML -notmatch "Library"){return $false}
-     for($i = 1; $i -lt $trs.length; $i++){
+    $results = Invoke-WebRequest $url
+    if($results -match "This search returned no results."){return $false}
+
+    #get availabilityDiv ids for all libraries that have a copy
+    $trs = $results.ParsedHtml.getElementsByTagName("tr")
+    if($trs[0].innerHTML -notmatch "Library"){return $false}
+    for($i = 1; $i -lt $trs.length; $i++){
         $lname = $trs[$i].firstChild.innerHTML.Trim();
-        $id = ($trs[$i].lastChild.firstChild.id | Select-String "availabilityDiv(.*)").Matches.Groups[1].Value
+        $id = ($trs[$i].lastChild.firstChild.id 
+            | Select-String "availabilityDiv(.*)").Matches.Groups[1].Value
         $LibraryIds[$lname] = $id
-     }
- 
+    }
 
-     $availabilityJson = ($results.Scripts[-1].InnerHtml | Select-String "(?smi)^parseDetailAvailabilityJSON\(({.*?})\);").Matches[0].Groups[1]
-     $availabilityObj = ConvertFrom-Json $availabilityJson
 
-     #if the library isn't in the list, it doesn't even have a copy.
-     if(!$LibraryIds.ContainsKey($library)){return $false}
-     $idx = $availabilityObj.ids.IndexOf($LibraryIds[$library])
-     if($idx -eq -1){ return $false}
-     $str = $availabilityObj.strings[$idx]
+    $availabilityJson = ($results.Scripts[-1].InnerHtml 
+        | Select-String "(?smi)^parseDetailAvailabilityJSON\(({.*?})\);").Matches[0].Groups[1]
+    $availabilityObj = ConvertFrom-Json $availabilityJson
 
-     # if it's "Due", then it's not available. AFAIK, that's the only unavailable state.
-     # ergo otherwise it's available! yay!
-     if($str.StartsWith("Due")){return $false}else{return $true}
- }
+    #if the library isn't in the list, it doesn't even have a copy.
+    if(!$LibraryIds.ContainsKey($library)){return $false}
+    $idx = $availabilityObj.ids.IndexOf($LibraryIds[$library])
+    if($idx -eq -1){ return $false }
+    $str = $availabilityObj.strings[$idx]
 
- function GetOtherEditions
- {  param([string]$isbn)
-    
+    # if it's "Due", then it's not available. AFAIK, that's the only unavailable state.
+    # ergo otherwise it's available! yay!
+    if($str.StartsWith("Due")){return $false}else{return $true}
+}
+
+function GetOtherEditions
+{  param([string]$isbn)
+
     $url="http://xisbn.worldcat.org/webservices/xid/isbn/$isbn"
     $response = Invoke-RestMethod $url
     return $response.rsp.isbn
- }
+}
 
- if(CheckLibrary $isbn){
+######################################
+## main
+######################################
+
+if(CheckLibrary $isbn){
     return $true
- }else{
+}else{
     $otherEditions = GetOtherEditions $isbn
     foreach($other in $otherEditions){
         if(CheckLibrary $other)
         {
             echo "found alternate edition: $other"
-            return $true
+                return $true
         }
     }
     return $false
- }
- 
+}
+
