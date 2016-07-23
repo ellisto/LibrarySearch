266f8ff243809c9e8264e3402990316787f65b80
TJ Ellis <TJ Ellis>
2016-07-23 19:12:07 -0400

    updated to check all editions

--- librarySearch.js
+++ librarySearch.js
@@ -1,15 +1,18 @@
 function parseResponse(response,library){
     console.log("parsing response...");
 
+    if(typeof(response) === "undefined"){return false;}
+
     var noresults = "This search returned no results.";
     var idx = response.indexOf(noresults);
-    console.log("idx: " + idx);
+    //console.log("idx: " + idx);
     if(idx != -1){
         return false;
     }
     var newDoc = document.implementation.createHTMLDocument();
     var parsedHtml = $.parseHTML(response,newDoc,true);
     var trs = $("tr", parsedHtml);
+    if(trs.length == 0){return false;}
 
     //if first row doesn't have a "Library" header, we didnt get the right result page
     if(trs[0].innerHTML.indexOf("Library") == -1){
@@ -56,13 +59,40 @@ function getLibraryIds(trs){
     return libraryIds;
 }
 
+function checkOtherEditions(isbn,library,callback){
+    url = "http://xisbn.worldcat.org/webservices/xid/isbn/" + isbn;
+    return $.get(url).then(function(data){
+        var otherEditions = $("isbn",data).contents();
+        var otherRequests = [];
+        for( i = 0; i < otherEditions.length; i++){
+            var ed = otherEditions[i].textContent;
+            console.log("checking ed: " + ed);
+            var req = checkLibrary(ed,library);
+            req.done(function(data){
+                found = parseResponse(data,library);
+                if(found){
+                    console.log("found alternate edition: " + ed);
+                }
+                callback(found);
+            });
+        }
+    });
+}
 
-function checkLibrary(isbn, library, callback, error){
+
+function checkLibrary(isbn,library){
     var url = "http://mdpl.ent.sirsi.net/client/catalog/search/results?qu=" + isbn;
     console.log("requesting url=" + url);
-    var request = $.ajax(url); 
+    return $.ajax(url); 
+}
+
+function checkLibraryForAllEditions(isbn, library, callback, error){
+    var request = checkLibrary(isbn, library);
     request.done(function(data){
-        var parsed = parseResponse(data,library);
-        callback(parsed);
+        var found = parseResponse(data,library);
+        if(!found){
+           checkOtherEditions(isbn,library,callback);
+        }  
+        callback(found); 
     }).fail(error);
 }
